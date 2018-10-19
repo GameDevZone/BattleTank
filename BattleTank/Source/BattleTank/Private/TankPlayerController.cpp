4895464a5a1cdee81e699a06d77d588ceeb98b14
zhyrao <rain330047@outlook.com>
2018-10-19 22:46:00 +0800

    Find the Screen Pixel Coord of Cross Hair

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -24,10 +24,42 @@ void ATankPlayerController::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	// Aim Towards CrossHair
+	AimTowardsCrossHair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrossHair()
+{
+	if (!GetControlledTank())
+		return;
+
+	FVector HitLocation;	// OUT Parameter
+
+	if (GetSighRayHitLocation(HitLocation))
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+
+			// tell controlled tank to aim at this point
+	}
+	
+}
+// get world location if line trace through cross hair, true if it hits the landscape 
+bool ATankPlayerController::GetSighRayHitLocation(FVector& HitLocation) const
+{
+	// Find cross hair position
+	int32 ViewPortSizeX, ViewPortSizeY;
+	GetViewportSize(ViewPortSizeX, ViewPortSizeY);
+	UE_LOG(LogTemp, Warning, TEXT("ViewPortSizeX: %d"), ViewPortSizeX);
+	UE_LOG(LogTemp, Warning, TEXT("ViewPortSizeY: %d"), ViewPortSizeY);
+	
+	FVector2D ScreenLocation = FVector2D(ViewPortSizeX * CrossHairXLocation, ViewPortSizeY * CrossHairYLocation);
+
+	// De-project the screen position to world position
+	// line trace along the look direction, and see what we hit
+	return true;
 }
 
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,6 +14,12 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "CrossHair")
+	float CrossHairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere, Category = "CrossHair")
+	float CrossHairYLocation = 0.3333f;
 	
 	
 public:
@@ -23,4 +29,12 @@ public:
 
 	// Get Player Controller Controlled Tank
 	ATank * GetControlledTank() const;	
+
+	// start the tank move the barrel so that a shot would it 
+	// where the cross hair intersects the world
+	void AimTowardsCrossHair();
+
+private:
+	// Return an Out Parameter, true if hit landscape
+	bool GetSighRayHitLocation(FVector& HitLocation) const;
 };
